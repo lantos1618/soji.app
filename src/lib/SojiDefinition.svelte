4d17569764cf5d3a1bf500442b08528d55a2d3d0
lantos-lgtm <l.leong1618@gmail.com>
2022-06-25 12:08:56 +0100

    +box +sojidef

---
+++ src/lib/SojiDefinition.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Box from './Box.svelte';
+</script>
+
+<Box>
+    <div>
+        <div>
+            <h1>Soji</h1>
+            <h1>/səʊdʒi/</h1>
+        </div>
+    </div>
+    <span class="marker">noun</span>
+    <h5>soji; plural noun: sojis;</h5>
+    <h5 class="text">sound + emoji = Soji</h5>
+    <h5>2020s: Soji is a Japanese word for "sound + emoji</h5>
+</Box>
+
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
 import App from './App.svelte'
 
+declare global {
+  interface Window {
+    ethereum: any;
+  }
+}
+
 const app = new App({
   target: document.getElementById('app')
 })
