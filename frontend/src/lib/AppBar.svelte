93f1f0b3f34ed47d931fd59af20cf41ba2465eb1
lantos-lgtm <l.leong1618@gmail.com>
2022-06-26 15:18:05 +0100

    +ethers connect

--- frontend/src/lib/AppBar.svelte
+++ frontend/src/lib/AppBar.svelte
@@ -1,22 +1,36 @@
 <script lang="ts">
-    import { connected, signer, signerAddress, defaultEvmStores } from 'svelte-ethers-store'
-
+    import {
+        connected,
+        signer,
+        signerAddress,
+        defaultEvmStores,
+    } from "svelte-ethers-store";
 </script>
 
-<nav class="flex items-center justify-between border bg-indigo-400 sticky p-2">
-    <span>Soji</span>
-    <ul class="flex ">
-        <li class="mr-2">
-            <a on:click={() => console.info("about!")}>about</a>
-        </li>
-        {#if $connected}
-        <li class="mr-2 overflow-ellipsis">
-            {$signerAddress}
-        </li>
-        {:else}
-        <li class="mr-2">
-            <a on:click={() => defaultEvmStores.setProvider()}>connect</a>
-        </li>
-        {/if}
-    </ul>
+<nav class="flex bg-indigo-400 sticky p-2">
+    <div class="flex justify-between mx-auto w-3/4">
+        <span>Soji</span>
+        <ul class="flex ">
+            <li class="mr-2">
+                <button on:click={() => console.info("about!")}>about</button>
+            </li>
+            {#if $connected}
+                <li class="mr-2 truncate">
+                    {$signerAddress}
+                </li>
+            {:else}
+                <li class="mr-2">
+                    <button on:click={() => defaultEvmStores.setProvider()}
+                        >connect</button
+                    >
+                </li>
+            {/if}
+        </ul>
+    </div>
 </nav>
+
+<style>
+    li {
+        max-width: 100px;
+    }
+</style>
