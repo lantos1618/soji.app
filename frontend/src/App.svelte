629b3b88a70d6af9f0be5d010dc9ab6d348aec07
lantos-lgtm <l.leong1618@gmail.com>
2022-06-26 15:19:23 +0100

    app+sojiupload app+sojiSearchBar

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,15 +1,26 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  import { defaultEvmStores } from "svelte-ethers-store";
+
   import AppBar from "./lib/AppBar.svelte";
 
   import SojiDefinition from "./lib/SojiDefinition.svelte";
   import Sojis from "./lib/Sojis.svelte";
+import SojiSearchBar from "./lib/SojiSearchBar.svelte";
+  import SojiUpload from "./lib/SojiUpload.svelte";
+
+  onMount((): void => {
+    defaultEvmStores.setProvider();
+  });
 </script>
 
-<div class="w-full">
+<div class="w-full flex flex-col">
   <AppBar />
-  <div class="container mx-auto py-5">
-    <main class="w-full">
+  <div class="flex mx-auto py-5">
+    <main class="flex flex-col space-y-3">
       <SojiDefinition />
+      <SojiUpload />
+      <SojiSearchBar />
       <Sojis />
     </main>
   </div>
