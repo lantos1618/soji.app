e660181f9e474b737be0e4c9f4ae8fc3e45915ba
lantos-lgtm <l.leong1618@gmail.com>
2022-06-26 15:18:19 +0100

    format soji

--- frontend/src/lib/Soji.svelte
+++ frontend/src/lib/Soji.svelte
@@ -1,30 +1,36 @@
 <script lang="ts">
-    import { onMount } from "svelte";
+    import type { SojiType } from "./store";
+    export let soji: SojiType;
     
-        import type {iSoji} from "./store";
-        export let soji: iSoji;
-        var sojiAudio = new Audio(soji.animation_url);
-    
-    
-        let sojiAudioPlaying = false;
-        sojiAudio.onplaying = () => {
-            sojiAudioPlaying = true;
-        };
-        sojiAudio.onpause = () => {
-            sojiAudioPlaying = false;
-        };
-        function handleSojiSoundClick() {
-            sojiAudio.play();
-        }
-    
-        function play() {
-            sojiAudio.play();
-        }
-    
-    </script>
-    
-    
-    <div on:click={play} class="border flex items-center p-2 {sojiAudioPlaying ? 'bg-blue-300' : 'bg-transparent'} rounded">
-        <img class="rounded" width="50px" height="50px" src={soji.image} alt={soji.name} />
-        <span class="px-2">{soji.name}</span>
-    </div>
+    var sojiAudio = new Audio(soji.animation_url);
+
+    let sojiAudioPlaying = false;
+    sojiAudio.onplaying = () => {
+        sojiAudioPlaying = true;
+    };
+    sojiAudio.onpause = () => {
+        sojiAudioPlaying = false;
+    };
+
+    function play() {
+        sojiAudio.play();
+    }
+</script>
+
+<div
+    on:click={play}
+    class="border flex items-center p-2 
+    {sojiAudioPlaying
+        ? 'bg-blue-300'
+        : 'bg-transparent'}
+    rounded"
+>
+    <img
+        class="rounded"
+        width="50px"
+        height="50px"
+        src={soji.image}
+        alt={soji.name}
+    />
+    <span class="px-2">{soji.name}</span>
+</div>
