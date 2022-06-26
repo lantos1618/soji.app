f852a87ec3689a759d3a0cffc101257f245d155b
lantos-lgtm <l.leong1618@gmail.com>
2022-06-26 15:18:35 +0100

    format sojis

--- frontend/src/lib/Sojis.svelte
+++ frontend/src/lib/Sojis.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
     import Soji from "./Soji.svelte";
-    import { sojis } from "./store";
+    import { sojisSearchResult } from "./store";
 </script>
 
-<div class="container mx-auto">
-    <div class="grid py-2 sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-8 gap-4 grid-flow-row-dense">
-        {#each $sojis as soji}
+<div
+    class="grid 
+    py-2 
+    sm:grid-cols-2 
+    md:grid-cols-4
+    xl:grid-cols-8
+    gap-4
+    grid-flow-row-dense"
+>
+    {#each $sojisSearchResult as soji}
         <Soji bind:soji />
-        {/each}
-    </div>
+    {/each}
 </div>
